2c60f01e485a1c2d389d46aededd374308737777
Alexander Lesin <alexander.lesin@openiam.com>
2017-08-02 12:41:39 -0700

    basic custom menu

--- SideMenu/CustomMenu/CustomMenu/ViewController.swift
+++ SideMenu/CustomMenu/CustomMenu/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftLeading: NSLayoutConstraint!
+    @IBOutlet weak var menuContainter: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        leftLeading.constant = -menuContainter.frame.width
+        self.view.layoutIfNeeded()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func showMenu(_ sender: UIBarButtonItem) {
+        if (leftLeading.constant != 0 )
+        {
+            leftLeading.constant = 0
+            UIView.animate(withDuration: 0.5, animations: {self.view.layoutIfNeeded()})
+        } else {
+            leftLeading.constant = -menuContainter.frame.width
+            UIView.animate(withDuration: 0.5, animations: {self.view.layoutIfNeeded()})
+        }
+    }
 }
 
