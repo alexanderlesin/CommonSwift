f0152322cda03417f89101283970b0b1750c283e
Alexander Lesin <alexander.lesin@openiam.com>
2017-08-03 07:48:14 -0700

    add rounded border

--- SideMenu/CustomMenu/CustomMenu/ViewController.swift
+++ SideMenu/CustomMenu/CustomMenu/ViewController.swift
@@ -20,8 +20,33 @@ class ViewController: UIViewController {
         // Remove the Navigation Bar Border
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
+        
+        // 
+//        menuContainter.layer.borderColor = UIColor.red.cgColor
+//        menuContainter.layer.borderWidth = 1
     }
     
+    override func viewDidLayoutSubviews() {
+//        menuContainter.layer.sublayers = nil
+        // Add rounded corner
+        let path = UIBezierPath(roundedRect: menuContainter.bounds, byRoundingCorners: [.topRight], cornerRadii: CGSize(width: 10, height: 10))
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = path.cgPath
+        menuContainter.layer.mask = maskLayer
+        
+        // Add border
+        let borderLayer = CAShapeLayer()
+        borderLayer.path = maskLayer.path // Reuse the Bezier path
+        borderLayer.fillColor = UIColor.clear.cgColor
+        borderLayer.strokeColor = UIColor.lightGray.cgColor
+        borderLayer.lineWidth = 1
+        borderLayer.frame = menuContainter.bounds
+        menuContainter.layer.addSublayer(borderLayer)
+        
+        
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
